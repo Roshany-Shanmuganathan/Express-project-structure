c61e68494e0ae9c644c743b24fd04cb27c9e3f80
Roshany Shanmuganathan <s.roshany7@gmail.com>
2025-09-29 15:13:32 +0530

    All usesrs created in app.js

--- app.js
+++ app.js
@@ -2,20 +2,34 @@
 import express from "express";
 
 const app = express();
-const PORT = 4000;
-// user route created using express
 const router = express.Router();
+
+const PORT = 3000;
 
 // HTTP method handling
 app.get("/", (req, res) => {
   res.send("Hello Express!");
 });
 
-router.get("/user", (req, res) => {
-  res.send("This is GET user");
+// users routes
+router.get("/users", (req, res) => {
+  res.send("This is GET Users...");
+});
+router.get("/users/:id", (req, res) => {
+  res.send("This is GET one User...");
+});
+router.post("/users", (req, res) => {
+  res.send("This is Create Users...");
+});
+router.put("/users/:id", (req, res) => {
+  res.send("This is Update User...");
+});
+router.delete("/users/:id", (req, res) => {
+  res.send("This is Delete User...");
 });
 
 app.use(router);
+
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
