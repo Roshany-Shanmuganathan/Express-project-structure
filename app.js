47870fdb4b4b771c15c25389b643cf87bb4097c0
Roshany Shanmuganathan <s.roshany7@gmail.com>
2025-09-29 14:35:01 +0530

    user route created using express

--- app.js
+++ app.js
@@ -2,25 +2,20 @@
 import express from "express";
 
 const app = express();
-const PORT = 3000;
+const PORT = 4000;
+// user route created using express
+const router = express.Router();
 
 // HTTP method handling
 app.get("/", (req, res) => {
   res.send("Hello Express!");
 });
 
-app
-  .route("/book")
-  .get((req, res) => {
-    res.send("Get a random book");
-  })
-  .post((req, res) => {
-    res.send("Add a book");
-  })
-  .put((req, res) => {
-    res.send("Update the book");
-  });
+router.get("/user", (req, res) => {
+  res.send("This is GET user");
+});
 
+app.use(router);
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
